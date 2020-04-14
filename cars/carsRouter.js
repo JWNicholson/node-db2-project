af55168cae30f6a2dd27308c6cd0a966cbf947a2
JW Nicholson <john.nicholson812@gmail.com>
2020-04-14 17:54:33 -0400

    DELETE is working

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -66,25 +66,20 @@ router.put('/:id', (req, res) => {
         })
 });
 
-// router.delete('/:id', (req,res) => {
-//     db('cars')
-//         .where({id: req.params.id})
-//         .remove(req.params.id)
-//         .then(code => {
-//             if (code === undefined) {
-//               res
-//                 .status(202)
-//                 .json({ message: "This car has been successfully deleted." });
-//             } else {
-//               res.status(404).json({ message: "There isn't a car with this id." });
-//             }
-//           })
-//           .catch(err => {
-//             res
-//               .status(500)
-//               .json({ message: "There was an error deleting this car." });
-//           });
-// });
+router.delete('/:id', (req,res) => {
+    
+    db('cars')
+        .where({id: req.params.id})
+        .del(req.params.id)
+        .then(delCar => {
+           res.status(200).json(delCar)
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ message: "There was an error deleting this car." });
+          });
+});
 
 
 module.exports = router;
